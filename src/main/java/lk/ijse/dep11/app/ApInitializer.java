00300db937edc34296f3c9a03cab908d44c9e012
Kaveendra Randeera <khkrandeera@gmail.com>
2023-10-13 23:39:32 +0530

    Add AppInitializer and AppInitializerWrapper

---
+++ src/main/java/lk/ijse/dep11/app/ApInitializer.java
@@ -0,0 +1,24 @@
+package lk.ijse.dep11.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ApInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/MainForm.fxml"))));
+        primaryStage.setTitle("Color Chooser");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+        primaryStage.centerOnScreen();
+    }
+}
